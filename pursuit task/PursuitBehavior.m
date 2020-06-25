8d5fbe134d8a89470e5ddfcc0d96a954731b8f9f
nogalarry <noga.larry@mail.huji.ac.il>
2020-06-25 10:36:21 +0300

    different difrections in pursuit behavior

--- pursuit task/PursuitBehavior.m
+++ pursuit task/PursuitBehavior.m
@@ -2,7 +2,7 @@
 
 clear all
 
-MaestroPath = 'C:\Users\Noga\Music\';
+MaestroPath = 'C:\Users\Noga\Music\DATA\';
 supPath = 'C:\Users\Noga\Documents\Vermis Data';
 load ('C:\Users\Noga\Documents\Vermis Data\task_info');
 
@@ -48,3 +48,54 @@ figure
 scatter(mean(velHigh(:,200:250),2),mean(velLow(:,200:250),2))
 xlabel('High');ylabel('Low')
 refline(1,0)
+
+%% Seperated by direction
+
+clear all
+
+MaestroPath = 'C:\Users\Noga\Music\DATA\';
+supPath = 'C:\Users\Noga\Documents\Vermis Data';
+load ('C:\Users\Noga\Documents\Vermis Data\task_info');
+
+req_params.grade = 10;
+req_params.cell_type = 'CRB|PC';
+req_params.task = 'pursuit_8_dir_75and25';
+req_params.ID = 4000:5000;
+req_params.num_trials = 100;
+req_params.remove_question_marks =0;
+
+behavior_params.time_after = 300;
+behavior_params.time_before = 0;
+behavior_params.smoothing_margins = 100; % ms
+behavior_params.SD = 10; % ms
+
+lines = findLinesInDB (task_info, req_params);
+cells = findPathsToCells (supPath,task_info,lines);
+
+directions = 0:45:315;
+
+for ii = 1:length(cells)
+    
+    data = importdata(cells{ii});
+    data = getBehavior(data,MaestroPath);
+    [~,match_p] = getProbabilities (data);
+    [~,match_d] = getDirections (data);
+    boolFail = [data.trials.fail];
+    indLow = find (match_p == 25 & (~boolFail));
+    indHigh = find (match_p == 75 & (~boolFail));
+    
+    for d=1:length(directions)
+        indDir = find(match_d==directions(d));
+        velLow(ii,d,:) = meanVelocitiesRotated(data,behavior_params,intersect(indLow,indDir));
+        velHigh(ii,d,:) = meanVelocitiesRotated(data,behavior_params,intersect(indHigh,indDir));
+    end
+    
+end
+
+
+figure
+errorbar(directions,nanmean(mean(velHigh(:,:,200:250),3)),nanstd(mean(velHigh(:,:,200:250),3))/sqrt(length(cells)),'b');
+hold on
+errorbar(directions,nanmean(mean(velLow(:,:,200:250),3)),nanstd(mean(velLow(:,:,200:250),3))/sqrt(length(cells)),'r')
+xlabel('Vel');ylabel('Direction 200:25 ms')
+legend('P=25','P=75')
