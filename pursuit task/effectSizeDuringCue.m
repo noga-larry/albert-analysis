33f8b2778652679c96a57981d8ec9a0877eea6ac
nogalarry <noga.larry@mail.huji.ac.il>
2020-06-25 10:35:28 +0300

    cell types in cue effect size analysis

--- pursuit task/effectSizeDuringCue.m
+++ pursuit task/effectSizeDuringCue.m
@@ -3,14 +3,13 @@ supPath = 'C:\Users\Noga\Documents\Vermis Data';
 load ('C:\Users\Noga\Documents\Vermis Data\task_info');
 
 req_params.grade = 7;
-req_params.cell_type = 'PC cs';
-req_params.task = 'pursuit_8_dir_75and25';
+req_params.cell_type = 'PC ss|CRB';
+req_params.task = 'saccade_8_dir_75and25';
 req_params.ID = 4000:5000;
-req_params.num_trials = 50;
+req_params.num_trials = 20;
 req_params.remove_question_marks = 1;
 
 raster_params.align_to = 'cue';
-raster_params.cue_time = 500;
 raster_params.time_before = 299;
 raster_params.time_after = 500;
 raster_params.smoothing_margins = 0;
@@ -25,7 +24,8 @@ cells = findPathsToCells (supPath,task_info,lines);
 for ii = 1:length(cells)
     
     data = importdata(cells{ii});
-   
+    cellType{ii} = data.info.cell_type;
+
     [~,match_p] = getProbabilities (data);
     boolFail = [data.trials.fail] | ~[data.trials.previous_completed];
     match_p = match_p(find(~boolFail))';
@@ -56,5 +56,47 @@ xlabel('time')
 ylabel('reward+time*reward')
 title(['p_{reward} = ' num2str(p)])
 refline(1,0)
+%%
+figure;
+boolPC = strcmp('PC ss', cellType);
+
+scatter(omegaT(boolPC),omegaR(boolPC),'filled','k'); hold on
+p1 = signrank(omegaT(boolPC),omegaR(boolPC));
+scatter(omegaT(~boolPC),omegaR(~boolPC),'filled','m'); 
+p2 = signrank(omegaT(~boolPC),omegaR(~boolPC));
+xlabel('time')
+ylabel('reward+time*reward')
+refline(1,0)
+legend('PC ss','CRB')
+title(['PC ss: p_{reward} = ' num2str(p1) ', CRB: p_{reward} = ' num2str(p2)])
+ 
+figure;
+bins = -0.2:0.05:1;
+
+subplot(3,1,1)
+p = ranksum(omegaT(boolPC),omegaT(~boolPC))
+plotHistForFC(omegaT(boolPC),bins,'g'); hold on
+plotHistForFC(omegaT(~boolPC),bins,'r'); hold on
+legend('SS', 'CRB')
+title(['Time: ranksum: P = ' num2str(p) ', n_{ss} = ' num2str(sum(boolPC)) ', n_{crb} = ' num2str(sum(~boolPC))])
+xlabel ('Time effect size')
+bins = -0.2:0.01:1;
+
+subplot(3,1,2)
+p = ranksum(omegaR(boolPC),omegaR(~boolPC))
+plotHistForFC(omegaR(boolPC),bins,'g'); hold on
+plotHistForFC(omegaR(~boolPC),bins,'r'); hold on
+legend('SS', 'CRB')
+title(['Reward: ranksum: P = ' num2str(p) ', n_{ss} = ' num2str(sum(boolPC)) ', n_{crb} = ' num2str(sum(~boolPC))])
+xlabel ('Reward effect size')
+
+subplot(3,1,3)
+overallExplained = omegaR+omegaT;
+p = ranksum(overallExplained(boolPC),overallExplained(~boolPC))
+plotHistForFC(overallExplained(boolPC),bins,'g'); hold on
+plotHistForFC(overallExplained(~boolPC),bins,'r'); hold on
+legend('SS', 'CRB')
+title(['Over all: ranksum: P = ' num2str(p) ', n_{ss} = ' num2str(sum(boolPC)) ', n_{crb} = ' num2str(sum(~boolPC))])
+xlabel ('Total Explained')
 
 
