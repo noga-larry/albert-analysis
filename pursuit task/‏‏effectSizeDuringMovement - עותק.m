74e838f9c2a8210a84ea415692faadb40c74b3c7
nogalarry <noga.larry@mail.huji.ac.il>
2021-07-24 13:33:10 +0300

    no message

---
+++ pursuit task/‏‏effectSizeDuringMovement - עותק.m
@@ -0,0 +1,228 @@
+clear all
+supPath = 'C:\Users\Noga\Documents\Vermis Data';
+load ('C:\Users\Noga\Documents\Vermis Data\task_info');
+
+req_params.grade = 7;
+req_params.cell_type = 'PC ss|CRB';
+req_params.task = 'saccade_8_dir_75and25';
+req_params.ID = 4000:5000;
+req_params.num_trials = 50;
+req_params.remove_question_marks = 1;
+
+raster_params.align_to = 'targetMovementOnset';
+raster_params.time_before = 299;
+raster_params.time_after = 500;
+raster_params.smoothing_margins = 0;
+bin_sz = 50;
+
+ts = -raster_params.time_before:raster_params.time_after;
+
+lines = findLinesInDB (task_info, req_params);
+cells = findPathsToCells (supPath,task_info,lines);
+
+omegaT = nan(1,length(cells));
+omegaR = nan(1,length(cells));
+omegaD = nan(1,length(cells));
+
+list = [];
+for ii = 1:length(cells)
+    
+    
+    data = importdata(cells{ii});
+    cellType{ii} = data.info.cell_type;
+
+   
+    [~,match_p] = getProbabilities (data);
+    boolFail = [data.trials.fail] | ~[data.trials.previous_completed];
+    match_p = match_p(find(~boolFail))';
+    [~,match_d] = getDirections (data);
+    boolFail = [data.trials.fail] | ~[data.trials.previous_completed];
+    match_d = match_d(find(~boolFail))';
+    
+    raster = getRaster(data,find(~boolFail),raster_params);
+    response = reshape(raster,bin_sz,size(raster,1)/bin_sz,size(raster,2));
+    response = (squeeze(sum(response))/bin_sz)*1000;
+    
+    groupT = repmat((1:size(response,1))',1,size(response,2));
+    groupR = repmat(match_p',size(response,1),1);
+    groupD = repmat(match_d',size(response,1),1);
+
+    [p,tbl,stats,terms] = anovan(response(:),{groupT(:),groupR(:),groupD(:)},...
+        'model','full','display','off');
+    
+    totVar = tbl{10,2};
+    SSe = tbl{9,2};
+    msw = tbl{9,5};
+    N = length(response(:));
+    
+    omega = @(tbl,dim) (tbl{dim,2}-tbl{dim,3}*msw)/(tbl{dim,2}+(N-tbl{dim,3})*msw);
+      
+    omega = @(tbl,dim) (tbl{dim,2}-tbl{dim,3}*msw)/(msw+totVar);
+    
+    omegaT(ii) = omega(tbl,2);
+    omegaR(ii) = omega(tbl,3)+omega(tbl,5);
+    omegaD(ii) = omega(tbl,4)+omega(tbl,6);
+    omegaRD(ii) = omega(tbl,7)+omega(tbl,8);
+    
+    overAllExplained(ii) = (totVar - SSe)/totVar;
+    
+    if omegaD(ii)>0.2
+        list = [list, data.info.cell_ID];
+    end
+    
+    % organize for mati
+    omegaData(ii,1) = data.info.cell_ID;
+    omegaData(ii,2) = omegaR(ii);
+    omegaData(ii,3) = omegaD(ii);
+    omegaData(ii,4) = omegaT(ii);
+    omegaData(ii,5) = omegaRD(ii);
+end
+
+
+%%
+
+
+figure;
+subplot(3,1,1)
+scatter(omegaT,omegaR,'filled'); 
+p = signrank(omegaT,omegaR);
+xlabel('time')
+ylabel('reward+time*reward')
+refline(1,0)
+title(['p_{reward} = ' num2str(p)])
+
+subplot(3,1,2)
+scatter(omegaT,omegaD,'filled'); 
+p = signrank(omegaT,omegaD);
+title(['p_{movement} = ' num2str(p)])
+xlabel('$\eta^2$ time','interpreter','latex')
+ylabel('direction+time*direcion')
+ refline(1,0)
+
+
+subplot(3,1,3)
+scatter(omegaR,omegaD,'filled'); 
+p = signrank(omegaR,omegaD)
+title(['p = ' num2str(p)])
+xlabel('reward+time*reward')
+ylabel('direction+time*direcion')
+ refline(1,0)
+
+
+figure;
+
+bins = -0.1:0.02:1;
+plotHistForFC(omegaT,bins,'g'); hold on
+plotHistForFC(omegaR,bins,'r'); hold on
+plotHistForFC(omegaD,bins,'b'); hold on
+legend('T','R','D')
+
+%%
+figure;
+boolPC = strcmp('PC ss', cellType);
+
+subplot(3,1,1)
+scatter(omegaT(boolPC),omegaR(boolPC),'filled','k'); hold on
+p1 = signrank(omegaT(boolPC),omegaR(boolPC));
+scatter(omegaT(~boolPC),omegaR(~boolPC),'filled','m'); 
+p2 = signrank(omegaT(~boolPC),omegaR(~boolPC));
+xlabel('time')
+ylabel('reward+time*reward')
+refline(1,0)
+legend('PC ss','CRB')
+title(['PC ss: p_{reward} = ' num2str(p1) ', CRB: p_{reward} = ' num2str(p2)])
+
+
+subplot(3,1,2)
+scatter(omegaT(boolPC),omegaD(boolPC),'filled','k'); hold on
+p1 = signrank(omegaT(boolPC),omegaD(boolPC));
+scatter(omegaT(~boolPC),omegaD(~boolPC),'filled','m'); 
+p2 = signrank(omegaT(~boolPC),omegaD(~boolPC));
+xlabel('time')
+ylabel(' direction+time*direcion')
+refline(1,0)
+legend('PC ss','CRB')
+title(['PC ss: p_{movement} = ' num2str(p1) ', CRB: p_{movement} = ' num2str(p2)])
+
+subplot(3,1,3)
+scatter(omegaD(boolPC),omegaR(boolPC),'filled','k'); hold on
+p1 = signrank(omegaD(boolPC),omegaR(boolPC));
+scatter(omegaD(~boolPC),omegaR(~boolPC),'filled','m'); 
+p2 = signrank(omegaD(~boolPC),omegaR(~boolPC));
+xlabel('reward+time*reward')
+ylabel('direction+time*direcion')
+refline(1,0)
+legend('PC ss','CRB')
+title(['PC ss: p = ' num2str(p1) ', CRB: p = ' num2str(p2)])
+
+
+ 
+ranksum(omegaD(boolPC),omegaD(~boolPC))
+figure;
+bins = -0.2:0.05:1;
+
+subplot(3,1,1)
+p = ranksum(omegaD(boolPC),omegaD(~boolPC))
+plotHistForFC(omegaD(boolPC),bins,'g'); hold on
+plotHistForFC(omegaD(~boolPC),bins,'r'); hold on
+legend('SS', 'CRB')
+title(['Direction: ranksum: P = ' num2str(p) ', n_{ss} = ' num2str(sum(boolPC)) ', n_{crb} = ' num2str(sum(~boolPC))])
+
+subplot(3,1,2)
+p = ranksum(omegaT(boolPC),omegaT(~boolPC))
+plotHistForFC(omegaT(boolPC),bins,'g'); hold on
+plotHistForFC(omegaT(~boolPC),bins,'r'); hold on
+legend('SS', 'CRB')
+title(['Time: ranksum: P = ' num2str(p) ', n_{ss} = ' num2str(sum(boolPC)) ', n_{crb} = ' num2str(sum(~boolPC))])
+
+bins = -0.2:0.01:1;
+
+subplot(3,1,3)
+p = ranksum(omegaR(boolPC),omegaR(~boolPC))
+plotHistForFC(omegaR(boolPC),bins,'g'); hold on
+plotHistForFC(omegaR(~boolPC),bins,'r'); hold on
+legend('SS', 'CRB')
+title(['Time: ranksum: P = ' num2str(p) ', n_{ss} = ' num2str(sum(boolPC)) ', n_{crb} = ' num2str(sum(~boolPC))])
+
+figure;
+bins = -0.3:0.05:1;
+
+overallExplained = omegaR+omegaD+omegaT;
+p = ranksum(overallExplained(boolPC),overallExplained(~boolPC))
+plotHistForFC(overallExplained(boolPC),bins,'g'); hold on
+plotHistForFC(overallExplained(~boolPC),bins,'r'); hold on
+legend('SS', 'CRB')
+title(['Over all: ranksum: P = ' num2str(p) ', n_{ss} = ' num2str(sum(boolPC)) ', n_{crb} = ' num2str(sum(~boolPC))])
+
+
+
+%% CV and
+raster_params.align_to = 'cue';
+raster_params.time_before = 200;
+raster_params.time_after = 600;
+raster_params.smoothing_margins = 0;
+req_params.num_trials = 50;
+
+for ii = 1:length(cells)
+    data = importdata(cells{ii});
+    
+    cellType{ii} = data.info.cell_type;
+    
+    boolFail = [data.trials.fail] | ~[data.trials.previous_completed];
+    
+    raster = getRaster(data,find(~boolFail),raster_params);
+    FR(ii) = mean(mean(raster)*1000);
+    CV2(ii) = nanmean(getCV2(data,find(~boolFail),raster_params));
+    CV(ii) = nanmean(getCV(data,find(~boolFail),raster_params));
+end
+
+figure; 
+scatter(FR(boolPC),overallExplained(boolPC),'k'); hold on
+scatter(FR(~boolPC),overallExplained(~boolPC),'m'); hold on
+legend('SS', 'CRB')
+
+xlabel('FR')
+ylabel('Sum of effects')
+[r1,p1] = corr(FR(boolPC)',overallExplained(boolPC)','type','Spearman')
+[r2,p2] = corr(FR(~boolPC)',overallExplained(~boolPC)','type','Spearman')
+title(['SS : r = ' num2str(r1) ', ' num2str(p1) ', CRB : r = ' num2str(r2) ', ' num2str(p2)])
