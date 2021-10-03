d0af36941faa86ec5c82220eb60254d6ed1c5581
nogalarry <noga.larry@mail.huji.ac.il>
2021-10-03 09:55:24 +0300

    Combining the data organization code

--- DataOrganization.m
+++ DataOrganization.m
@@ -52,6 +52,107 @@ sheet = 1;
 xlRange = 'X2';
 xlswrite(filename,A,sheet,xlRange)
 
+
+%% Remove spikelets 
+
+clear
+
+[task_info,dataPath] = loadDBAndSpecifyDataPaths('Vermis');
+
+req_params.grade = 7;
+%req_params.task = 'choice';
+req_params.remove_question_marks = 0;
+%req_params.ID = 5436;
+req_params.num_trials = 20;
+req_params.remove_repeats = 0;
+lines = findCspkSspkPairs(task_info,req_params);
+
+for ii=1:length(lines)
+    cells = findPathsToCells (dataPath,task_info,[lines(1,ii),lines(2,ii)]);
+    data = importdata(cells{1});
+    data_cs = importdata(cells{2});
+    
+    data = removeCspkSpikelets(data,data_cs);
+    save(cells{1},'data');
+end
+
+%% Remove double detections
+
+clear
+
+[task_info,dataPath] = loadDBAndSpecifyDataPaths('Vermis');
+req_params.grade = 7;
+%req_params.task = 'choice';
+req_params.remove_question_marks = 0;
+%req_params.ID = 5436;
+req_params.num_trials = 20;
+req_params.remove_repeats = 0;
+req_params.cell_type = 'PC cs';
+
+lines = findLinesInDB(task_info,req_params);
+cells = findPathsToCells (dataPath,task_info,lines);
+
+for ii=1:length(lines)
+    data = importdata(cells{ii});
+    
+    data = removeCspkDoubleDetections(data);
+    save(cells{ii},'data');
+end
+%% Histograms of numbers of trials
+
+cell_types = {'PC cs', 'PC ss','CRB'};
+tasks = {'choice','pursuit_8_dir_75and25', 'saccade_8_dir_75and25','speed_2_dir_0,50,100'};
+
+req_params.grade = 7;
+req_params.num_trials = 20;
+req_params.remove_question_marks =0;
+
+for i=1:length(cell_types)
+    req_params.cell_type = cell_types{i};
+   for j=1:length(tasks)
+       req_params.task = tasks{j};
+       subplot(length(cell_types),length(tasks),(length(tasks))*(i-1)+j); hold on
+       [task_info] = loadDBAndSpecifyDataPaths('Golda');
+       lines = findLinesInDB (task_info, req_params);
+       num_trials = [task_info(lines).num_trials];
+       plotHistForFC(num_trials,15,'cdf', 'unNormalized','r*')
+       [task_info] = loadDBAndSpecifyDataPaths('Vermis');
+       lines = findLinesInDB (task_info, req_params);
+       num_trials = [task_info(lines).num_trials];
+       plotHistForFC(num_trials,15,'cdf', 'unNormalized','b*')
+       title([tasks{j} ', ' cell_types{i}],'Interpreter', 'none')
+       
+   end
+end
+
+legend('Golda','Albert')
+
+%% Pairs
+clear req_params
+[task_info,sup_dir_to,sup_dir_from, path_to_task_info] = ...
+    loadDBAndSpecifyDataPaths('Vermis');
+req_params.grade = 7;
+req_params.remove_question_marks = 0;
+req_params.num_trials = 50;
+req_params.remove_repeats = false; 
+
+task_info = listSimultaneousPairs(task_info,req_params);
+save (path_to_task_info,'task_info')
+
+clear req_params
+%req_params.task = {'pursuit_8_dir_75and25'};
+req_params.grade = 7;
+req_params.remove_question_marks = 0;
+req_params.num_trials = 50;
+req_params.remove_repeats = 0;
+
+req_params.cell_type = 'BG';
+lines1 = findLinesInDB (task_info, req_params);
+req_params.cell_type = 'PC ss|CRB'
+lines2 = findLinesInDB (task_info, req_params);
+
+pairs = findPairs(task_info,lines1,lines2,req_params.num_trials)
+
 %% cell distribusion figure
 figure;
 col=[ 'b' 'k' 'r'];
@@ -111,24 +212,4 @@ for c = 1:length(types)
 end
 
 legend(types)
-
-
-
-%%
-
-dir_data_from = 'C:\noga\TD complex spike analysis\Maestro Data';
-dir_data_to = 'C:\noga\TD complex spike analysis\Data'
-monkey = 'albert'
-focus_task = 'pursuit_8_dir_75and25';
-get_data(task_info, dir_data_from, dir_data_to, monkey, focus_task)
-get_extended_data(dir_data_from, dir_data_to)
-%%
-% raster params
-raster_params.time_after = 1000; % ms, time after cue appearance of display.
-raster_params.time_before = 399; % ms, time before cue appearance of display.
-raster_params.plot_cell = 0; % whether or not to plot individual cells rasters and psths
-effect_window = (100:300) + raster_params.time_before; % time window to average the effect in (used in scatter plot and statistica tests)
-raster_params.include_failed = 1; % include failed trials
-raster_params.smoothing_margins = 100; % ms
-raster_params.SD = 10;
 
