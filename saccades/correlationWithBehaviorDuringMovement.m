0bce4d2b62423045c60f926277838cd12c58c60a
nogalarry <noga.larry@mail.huji.ac.il>
2022-01-17 07:11:59 +0200

    correlation with behavior

--- saccades/correlationWithBehaviorDuringMovement.m
+++ saccades/correlationWithBehaviorDuringMovement.m
@@ -1,17 +1,18 @@
-clear all
-supPath = 'C:\noga\TD complex spike analysis\Data\albert\pursuit_8_dir_75and25';
-MaestroPath = 'C:\Users\Owner\Desktop\DATA\albert\';
-load ('C:\noga\TD complex spike analysis\task_info');
+clear
+
+[task_info,supPath,MaestroPath] = loadDBAndSpecifyDataPaths('Vermis');
+PROBABILITIES = [25, 75];
+DIRECTIONS = 0:45:315;
 
 req_params.grade = 7;
-req_params.cell_type = 'PC ss';
-req_params.task = 'pursuit_8_dir_75and25';
-req_params.ID = 4000:5000;
-req_params.num_trials = 100;
+req_params.cell_type = {'PC ss', 'CRB','SNR','BG msn'};
+req_params.task = 'saccade_8_dir_75and25';
+req_params.ID = 4000:6000;
+req_params.num_trials = 120;
 req_params.remove_question_marks = 1;
 
-raster_params.allign_to = 'cue';
-raster_params.time_before = 399;
+raster_params.align_to = 'cue';
+raster_params.time_before = 0;
 raster_params.time_after = 700;
 raster_params.SD = 10;
 raster_params.smoothing_margins = 0;
@@ -19,52 +20,50 @@ raster_params.smoothing_margins = 0;
 lines = findLinesInDB (task_info, req_params);
 cells = findPathsToCells (supPath,task_info,lines);
 
-
 for ii = 1:length(cells)
+    
+    
     data = importdata(cells{ii});
     data = getBehavior(data,MaestroPath);
     
-    [~,match_d] = getDirections(data);
+    cellType{ii} = data.info.cell_type;
+    cellID(ii) = data.info.cell_ID;
+    
     boolFail = [data.trials.fail];
+    [~,match_p] = getProbabilities (data);
+    [~,match_d] = getDirections (data);
     
-    inxLow = find (match_p == 25 & (~boolFail));
-    inxHigh = find (match_p == 75 & (~boolFail));
-    
-    TCHigh = getTC(data, directions,inxHigh, comparison_window);
-    TCLow = getTC(data, directions,inxLow, comparison_window);
-    
-    spikesHigh = [];
-    spikesLow = [];
-    RTsHigh = [];
-    RTsLow = [];
-    for d = 1:length(directions)
+    for p = 1:length(PROBABILITIES)
         
-        inx = find (match_d == directions(d)|(~boolFail));
-
-        rasterHigh = getRaster(data,intersect(inx,inxHigh), raster_params);
-        rasterLow = getRaster(data, intersect(inx,inxLow), raster_params);
-        spikesHigh = [spikesHigh, mean(rasterHigh)*1000 - TCHigh(d)];
-        spikesLow = [spikesLow, mean(rasterLow)*1000 - TCLow(d)];
         
-        RTs = saccadeRTs(data,intersect(inx,inxHigh));
-        RTsHigh = [RTsHigh,RTs - mean(RTs)];
-        RTs = saccadeRTs(data,intersect(inx,inxLow));
-        RTsLow = [RTsLow,RTs - mean(RTs)];
+        spikes =[];
+        RTs =[];
         
-       
+        % substract RT average in direction to reduce directional variance
+        for d = 1:length(DIRECTIONS)
+            inx = find (match_p == PROBABILITIES(p) & (~boolFail)...
+                & match_d == DIRECTIONS(d));
+            
+            raster = getRaster(data,inx, raster_params);
+            spikes = [spikes, mean(raster)*1000];
+            
+            RTs_dir = saccadeRTs(data,inx);
+            RTs = [RTs, RTs_dir - nanmean(RTs_dir)];
+        end
+        correlationHigh(p,ii) = corr(spikes',RTs','Rows','Pairwise');
     end
-     correlationHigh(ii) = corr(spikesHigh',RTsHigh');
-     correlationLow(ii) = corr(spikesLow',RTsLow');
-    
     
 end
 
+%%
+figure; hold on
+bins = -1:0.1:1;
+for i = 1:length(req_params.cell_type)
+    
+    indType = find(strcmp(req_params.cell_type{i}, cellType));
+    plotHistForFC(squeeze(correlationHigh(1,indType)),bins);
+    disp(signrank(squeeze(correlationHigh(1,indType))))
+end
 
 
-
-figure;
-scatter(correlationHigh,correlationLow)
-signrank(correlationHigh,correlationLow)
-
-refline(1,0)
 xlabel('High'); ylabel('Low')
