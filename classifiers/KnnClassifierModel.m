429994c1b02f251a161f691f1b2c76d0353a2377
nogalarry <noga.larry@mail.huji.ac.il>
2022-01-25 17:35:58 +0200

    creat classifiers folder

---
+++ classifiers/KnnClassifierModel.m
@@ -0,0 +1,25 @@
+classdef KnnClassifierModel < ClassifierModel
+    
+    properties
+        NumNeighbors
+        knnObject
+    end
+    
+    methods
+        
+        function mdl = KnnClassifierModel(k)
+            if nargin > 0
+                mdl.NumNeighbors = k;
+            end
+        end
+        
+        function mdl = train(mdl,X,y)
+            mdl.knnObject = fitcknn(sum(X)',y,'NumNeighbors',mdl.NumNeighbors);
+        end
+        
+        function preds = predict(mdl,X,y)
+            preds = mdl.knnObject.predict(sum(X)');
+        end
+        
+    end
+end
