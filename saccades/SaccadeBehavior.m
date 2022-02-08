50f7472f2edc1e6b640d31d47fdf518f0f72fcff
nogalarry <noga.larry@mail.huji.ac.il>
2022-02-08 17:08:10 +0200

    saccade behavior, add frac previous failed

--- saccades/SaccadeBehavior.m
+++ saccades/SaccadeBehavior.m
@@ -2,28 +2,23 @@
 
 clear
 
-[task_info,supPath,MaestroPath] = ...
+[task_info,supPath] = ...
     loadDBAndSpecifyDataPaths('Vermis');
 
 req_params.grade = 7;
-req_params.cell_type = 'CRB|PC ss';
+req_params.cell_type = {'PC ss', 'PC cs', 'CRB','SNR','BG msn'};
 req_params.task = 'saccade_8_dir_75and25';
 %req_params.ID = setdiff([5600:6000],[5574,5575]);
 req_params.ID = 4000:6000;
 req_params.num_trials = 70;
 req_params.remove_question_marks =1;
 
-behavior_params.time_after = 300;
-behavior_params.time_before = 0;
-behavior_params.smoothing_margins = 100; % ms
-behavior_params.SD = 10; % ms
-
 lines = findLinesInDB(task_info,req_params);
 cells = findPathsToCells (supPath,task_info,lines);
 DIRECTIONS = [0:45:315];
 for ii = 1:length(cells)
     data = importdata(cells{ii});
-    data = getBehavior(data,MaestroPath);
+    data = getBehavior(data,supPath);
     
     cellID(ii) = data.info.cell_ID;
     [~,match_p] = getProbabilities (data);
@@ -50,7 +45,7 @@ end
 
 %%
 
-ind = find(cellID < 5000);
+ind = find(cellID < inf);
 figure;
 subplot(2,2,1)
 aveLow = nanmean(RTLow(ind,:));
@@ -156,14 +151,10 @@ req_params.ID = 4000:6000;
 req_params.num_trials = 120;
 req_params.remove_question_marks =0;
 
-behavior_params.time_after = 300;
-behavior_params.time_before = 0;
-behavior_params.smoothing_margins = 100; % ms
-behavior_params.SD = 10; % ms
+
 
 lines = findLinesInDB (task_info, req_params);
 cells = findPathsToCells (supPath,task_info,lines);
-ts = -behavior_params.time_before:behavior_params.time_after;
 cellID = nan(length(cells),1);
 
 RTLow = nan(length(cells),length(DIRECTIONS),4);
@@ -275,3 +266,61 @@ p = signrank(com1(:),com2(:));
 refline(1,0)
 xlabel('Previous 75NR'); ylabel('Previous 75R')
 title(['75 in this trial, p =' num2str(p)])
+
+%% Frection of previous trial failures
+
+clear
+
+PROBABILITEIS = [25, 75];
+
+[task_info,supPath] = ...
+    loadDBAndSpecifyDataPaths('Vermis');
+
+req_params.grade = 7;
+req_params.cell_type = {'PC ss', 'PC cs', 'CRB','SNR','BG msn'};
+req_params.task = 'saccade_8_dir_75and25';
+%req_params.ID = setdiff([5600:6000],[5574,5575]);
+req_params.ID = 4000:6000;
+req_params.num_trials = 70;
+req_params.remove_question_marks =1;
+
+lines = findLinesInDB(task_info,req_params);
+cells = findPathsToCells (supPath,task_info,lines);
+
+for ii = 1:length(cells)
+    data = importdata(cells{ii});
+    
+    
+    cellID(ii) = data.info.cell_ID;
+    [~,match_p] = getProbabilities (data);
+    
+    
+    
+    for p = 1:length(PROBABILITEIS)
+        ind = find(~[data.trials.fail] & match_p == PROBABILITEIS(p));
+        frac(ii,p) = mean([data.trials(ind).previous_completed]);
+    end
+end
+
+%% 
+figure;
+subplot(2,1,1)
+ind = find(cellID>5000);
+scatter(frac(ind,2),frac(ind,1))
+refline(1,0)
+p = signrank(frac(ind,2),frac(ind,1));
+title(['Golda: p = ' num2str(p)])
+xlabel('P=75')
+ylabel('P=25')
+
+
+subplot(2,1,2)
+ind = find(cellID<5000);
+scatter(frac(ind,2),frac(ind,1))
+refline(1,0)
+p = signrank(frac(ind,2),frac(ind,1));
+title(['Albert: p = ' num2str(p)])
+xlabel('P=75')
+ylabel('P=25')
+
+
