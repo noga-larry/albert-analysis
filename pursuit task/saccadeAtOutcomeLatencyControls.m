454096be85cc8b95b77ae934e4a5769bb747a83a
nogalarry <noga.larry@mail.huji.ac.il>
2023-03-25 11:55:33 +0300

    saccades after reward

--- pursuit task/saccadeAtOutcomeLatencyControls.m
+++ pursuit task/saccadeAtOutcomeLatencyControls.m
@@ -2,37 +2,35 @@ clear
 
 [task_info,supPath,MaestroPath] = loadDBAndSpecifyDataPaths('Vermis');
 
-req_params = reqParamsEffectSize("both");
+TASK= "pursuit";
+MONKEY = "golda";
+req_params = reqParamsEffectSize(TASK,MONKEY);
 
 behavior_params.time_after = 1500;
 behavior_params.time_before = 1000;
 behavior_params.smoothing_margins = 100; % ms
 behavior_params.SD = 15; % ms
 
+ts = -behavior_params.time_before:behavior_params.time_after;
+DIRECTIONS = 0:45:360;
 
 windowEvent = -behavior_params.time_before:behavior_params.time_after;
-directionBorders = 0:45:360;
-timeAfterRewardForDirectionHist = 500;
+
 
 lines = findLinesInDB(task_info,req_params);
 cells = findPathsToCells (supPath,task_info,lines);
 
+
 for ii = 1:length(cells)
 
     data = importdata(cells{ii});
-    data = getExtendedBehavior(data,supPath,MaestroPath);
-    if any(data.extended_caliberation.R_squared<0.99)
-        disp('Bad Cab')
-    end
-
+    data = getExtendedBehavior(data,supPath);
 
     cellID(ii) = data.info.cell_ID;
 
-    % latency of first saccade
     % direction distribution of saccade
     % amplitude
-    % number of blinks
-    % number of saccades
+
     [~,match_p] = getProbabilities (data);
     [match_o] = getOutcome (data);
     boolFail = [data.trials.fail];
@@ -47,100 +45,96 @@ for ii = 1:length(cells)
     for j=1:length(inx)
 
 
-        rateBlink(ii,j,:) = eventRate(data,'extended_blink_begin','reward',inx{j},windowEvent); 
+        rateBlink(ii,j,:) = eventRate(data,'extended_blink_begin','reward',inx{j},windowEvent);
         saccadeRate(ii,j,:) = eventRate(data,'extended_saccade_begin','reward',inx{j},windowEvent);
 
-        firstSaccadeAmplitude = nan(length(inx),1);
-        firstSaccadeEndPoint = nan(length(inx),1);
-
-        alignmentTimes = alignmentTimesFactory(data,inx{j},'reward');
-
-        for t=1:length(inx{j})
-
-            if data.trials(inx{j}(t)).extended_trial_begin<1000 |...
-                    (length(data.trials(inx{j}(t)).extended_vPos)-data.trials(inx{j}(t)).trial_length...
-                    -data.trials(inx{j}(t)).extended_trial_begin <2000)
-                continue
-            end
-
-            saccadesAlignedToReward = data.trials(inx{j}(t)).extended_saccade_begin...
-                -data.trials(inx{j}(t)).rwd_time_in_extended;
-            saccadesAfterReward = saccadesAlignedToReward(saccadesAlignedToReward>0);
-            if isempty(saccadesAfterReward)
-                firstSaccadeLatency(t) = NaN;
-                disp('No saccade')
-                continue
-            end
-            firstSaccadeLatency(t) = min(saccadesAfterReward);
-
-        end
-
-
-        latency(1,ii) = nanmedian(firstSaccadeLatency(indLowR));
-        latency(2,ii) = nanmedian(firstSaccadeLatency(indHighR));
-        latency(3,ii) = nanmedian(firstSaccadeLatency(indLowNR));
-        latency(4,ii) = nanmedian(firstSaccadeLatency(indHighNR));
+        [ampitudes(ii,j,:),endPoints(ii,j,:,:)] = saccParams(data,inx{j});
 
     end
 
 end
 
 %%
-figure
-subplot(2,1,1)
-scatter(latencyLowR,latencyLowNR)
-p = signrank(latencyLowR,latencyLowNR);
-refline(1,0)
-xlabel('R'); ylabel('NR'); title(['25, p = ' num2str(p)])
-subplot(2,1,2)
-scatter(latencyHighR,latencyHighNR)
-p = signrank(latencyHighR,latencyHighNR);
-refline(1,0)
-xlabel('R'); ylabel('NR'); title(['75, p = ' num2str(p)])
+
+figure;
+subplot(2,2,1); hold on
+
+for i=1:size(saccadeRate,2)
+    ave = squeeze(mean(rateBlink(:,i,:)));
+    sem = squeeze(nanSEM(rateBlink(:,i,:)));
+    errorbar(ts,ave,sem)
+end
+
+legend({'P=25 R','P=75 R','P=25 NR','P=75 NR'})
+xlabel('time from outcome'); ylabel('blink rate')
+
+subplot(2,2,2); hold on
+for i=1:size(saccadeRate,2)
+    ave = squeeze(mean(saccadeRate(:,i,:)));
+    sem = squeeze(nanSEM(saccadeRate(:,i,:)));
+    errorbar(ts,ave,sem)
+end
+
+legend({'P=25 R','P=75 R','P=25 NR','P=75 NR'})
+xlabel('time from outcome'); ylabel('sacc rate')
+
+subplot(2,2,3); hold on
+for i=1:size(endPoints,2)
+    h = endPoints(:,i,:,1);
+    v = endPoints(:,i,:,2);
+    scatter(h(:),v(:),'filled')
+end
+legend({'P=25 R','P=75 R','P=25 NR','P=75 NR'})
+
+subplot(2,2,4); hold on
+for i=1:size(ampitudes,2)
+    x = ampitudes(:,i,:);
+    plotHistForFC(x,0:30)
+end
+legend({'P=25 R','P=75 R','P=25 NR','P=75 NR'})
+sgtitle(TASK + " , " + MONKEY)
 %%
-ind = find(latencyLowR<latencyHighR);
 
-aveLowR = nanmean(psthLowR(ind,:));
-semLowR =  nanstd(psthLowR(ind,:))/sqrt(length(ind));
-aveHighR = nanmean(psthHighR(ind,:));
-semHighR = nanstd(psthHighR(ind,:))/sqrt(length(ind));
+function [amp,endPoint] = saccParams(data,ind)
 
-aveLowNR = nanmean(psthLowNR(ind,:));
-semLowNR = nanstd(psthLowNR(ind,:))/sqrt(length(ind));
-aveHighNR = nanmean(psthHighNR(ind,:));
-semHighNR = nanstd(psthHighNR(ind,:))/sqrt(length(ind));
+DIRECTIONS = [0:45:315];
+amp = nan(length(DIRECTIONS),1);
+endPoint = nan(length(DIRECTIONS),2);
+[~,match_d] = getDirections(data);
 
-figure;
-subplot(2,1,1);
-errorbar(ts,aveLowR,semLowR,'r'); hold on
-errorbar(ts,aveHighR,semLowR,'b'); hold on
-xlabel('Time for reward')
-ylabel('rate (spk/s)')
-legend('25','75')
-title('Reward')
+for d=1:length(DIRECTIONS)
+    indDir = intersect(ind,find(match_d==DIRECTIONS(d)));
+    ampDir = nan(length(indDir),1);
+    endPointDir = nan(length(indDir),2);
+    for t=1:length(indDir)
+        saccInx = findFirstSacc(data,indDir(t));
+        if isnan(saccInx)
+            continue
+        end
+        hPos = data.trials(indDir(t)).hPos;
+        vPos = data.trials(indDir(t)).vPos;
+        tb = data.trials(indDir(t)).extended_saccade_begin(saccInx);
+        te = data.trials(indDir(t)).extended_saccade_end(saccInx);
+        ampDir(t) = sqrt((hPos(te)-hPos(tb))^2+(vPos(te)-vPos(tb))^2);
+        endPointDir(t,:) = [hPos(te),vPos(te)];
+    end
+amp(d) = mean(ampDir,'omitnan');
+endPoint(d,:) = mean(endPointDir,'omitnan');
+end
+end
 
-subplot(2,1,2);
-errorbar(ts,aveLowNR,semLowNR,'r'); hold on
-errorbar(ts,aveHighNR,semLowNR,'b'); hold on
-xlabel('Time for reward')
-ylabel('rate (spk/s)')
-legend('25','75')
-title('No Reward')
+%%
+function saccInx = findFirstSacc(data,ind)
 
-figure;
-subplot(2,1,1);
-scatter(mean(psthHighR(ind,compsrison_window),2),mean(psthLowR(ind,compsrison_window),2));
-refline(1,0)
-xlabel('75');ylabel('25')
-title('Reward')
-p = signrank(mean(psthHighR(ind,compsrison_window),2),mean(psthLowR(ind,compsrison_window),2))
-title(['Reward: p=' num2str(p) ', n=' num2str(length(ind))])
+    saccadesAlignedToReward = data.trials(ind).extended_saccade_begin...
+        -data.trials(ind).rwd_time_in_extended;
+    i = find(saccadesAlignedToReward>0,1);
+    if isempty(i)
+        disp('No saccade')
+        saccInx= nan;
+        return
+    end
+    saccInx= i;
 
+end
 
-
-subplot(2,1,2);
-scatter(mean(psthHighNR(ind,compsrison_window),2),mean(psthLowNR(ind,compsrison_window),2))
-refline(1,0)
-xlabel('75');ylabel('25')
-p = signrank(mean(psthHighNR(ind,compsrison_window),2),mean(psthLowNR(ind,compsrison_window),2))
-title(['No Reward: p=' num2str(p) ', n=' num2str(length(ind))])
