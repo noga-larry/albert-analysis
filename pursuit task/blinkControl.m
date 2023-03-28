1285194250a720e1c0ab4e9ce08a2e2e762fb7ce
nogalarry <noga.larry@mail.huji.ac.il>
2023-03-28 12:17:05 +0300

    Add blink control and contrastFigure

---
+++ pursuit task/blinkControl.m
@@ -0,0 +1,101 @@
+clear
+
+[task_info,supPath,MaestroPath] = loadDBAndSpecifyDataPaths('Vermis');
+
+TASK= "both";
+MONKEY = "both";
+EPOCH = 'reward';
+req_params = reqParamsEffectSize(TASK,MONKEY);
+
+behavior_params.time_after = 500;
+behavior_params.time_before = 0;
+behavior_params.smoothing_margins = 0; % ms
+
+windowEvent = -behavior_params.time_before:behavior_params.time_after;
+
+
+lines = findLinesInDB(task_info,req_params);
+cells = findPathsToCells (supPath,task_info,lines);
+cellType = cell(length(cells),1);
+cellID = nan(length(cells),1);
+
+for ii = 1:length(cells)
+
+    data = importdata(cells{ii});
+    cellType{ii} = task_info(lines(ii)).cell_type;
+    cellID(ii) = data.info.cell_ID;
+
+
+    data = getExtendedBehavior(data,supPath);
+
+    cellID(ii) = data.info.cell_ID;
+
+    boolFail = [data.trials.fail];
+    inx = find(~boolFail);
+    [~,mat] = eventRate(data,'extended_blink_begin','reward',...
+        inx,windowEvent,[]);
+
+
+    boolBlink = nan(1,length(data.trials));
+
+    boolBlink(find(boolFail)) = 0; % fail
+    boolBlink(inx(find(sum(mat,2)==0))) = 1; %no blink
+    boolBlink(inx(find(sum(mat,2)>0))) = 2; % blink
+
+    assert(~any(isnan(boolBlink)))
+    assert(length(boolBlink)==length(data.trials))
+
+    dataNoBlink.trials = data.trials(find(boolBlink==1));
+    dataBlink.trials = data.trials(find(boolBlink==2));
+
+    dataNoBlink.info = data.info;
+    dataBlink.info = data.info;
+
+    [effectSizesBlink(ii,:),ts] = effectSizeInTimeBin...
+        (dataBlink,EPOCH);
+
+    [effectSizesNoBlink(ii,:),ts] = effectSizeInTimeBin...
+        (dataNoBlink,EPOCH);
+
+end
+
+
+%%
+
+flds = fields(effectSizesBlink);
+
+
+h = cellID<inf% & rel
+
+figure; c=1;
+for f = 1:length(flds)
+
+    subplot(length(flds),2,c); hold on
+
+    for i = 1:length(req_params.cell_type)
+
+        indType = find(strcmp(req_params.cell_type{i}, cellType));
+
+        a = reshape([effectSizesBlink(indType,:).(flds{f})],length(indType),length(ts));
+
+        errorbar(ts,nanmean(a,1), nanSEM(a,1))
+
+    end
+    xlabel(['time from ' EPOCH ' (ms)' ])
+    title([flds{f} '- blink'], 'Interpreter', 'none')
+    c=c+1;
+    subplot(length(flds),2,c); hold on
+    for i = 1:length(req_params.cell_type)
+
+        indType = find(strcmp(req_params.cell_type{i}, cellType));
+
+        a = reshape([effectSizesNoBlink(indType,:).(flds{f})],length(indType),length(ts));
+        errorbar(ts,nanmean(a,1), nanSEM(a,1))
+
+    end
+    
+    xlabel(['time from ' EPOCH ' (ms)' ])
+    title([flds{f} ' -  No blink' ], 'Interpreter', 'none')
+    legend(req_params.cell_type)
+    c=c+1;
+end
---
+++ pursuit task/contrastFigure.m
@@ -0,0 +1,106 @@
+
+clear
+[task_info,supPath,~,task_DB_path] = loadDBAndSpecifyDataPaths('Vermis');
+
+EPOCH = 'reward';
+PLOT_CELL = false;
+
+
+req_params = reqParamsEffectSize("both","albert");
+
+lines = findLinesInDB (task_info, req_params);
+cells = findPathsToCells (supPath,task_info,lines);
+
+cellType = cell(length(cells),1);
+cellID = nan(length(cells),1);
+
+for ii = 1:length(cells)
+
+    data = importdata(cells{ii});
+    cellType{ii} = task_info(lines(ii)).cell_type;
+    cellID(ii) = data.info.cell_ID;
+
+
+    [effectSizes(ii,:),ts] = contrastInTimeBin...
+        (data,EPOCH,'prevOut',false,...
+        'velocityInsteadReward',false);
+end
+
+%%
+
+figure; hold on
+
+for i = 1:length(req_params.cell_type)
+
+    indType = find(strcmp(req_params.cell_type{i}, cellType));
+
+    errorbar(ts,nanmean(effectSizes(indType,:),1), nanSEM(effectSizes(indType,:)))
+
+
+
+end
+legend(req_params.cell_type)
+xlabel(['time from ' EPOCH ' (ms)' ])
+%%
+
+function [effectSizes, ts, low] = contrastInTimeBin(data,epoch,varargin)
+
+MINIMAL_RATE_IN_BIN = 0.001;
+
+p = inputParser;
+
+defaultPrevOut = false;
+addOptional(p,'prevOut',defaultPrevOut,@islogical);
+defaultVelocity = false;
+addOptional(p,'velocityInsteadReward',defaultVelocity,@islogical);
+defaultNumCorrectiveSaccades = false;
+addOptional(p,'numCorrectiveSaccadesInsteadOfReward',defaultNumCorrectiveSaccades,@islogical);
+
+
+parse(p,varargin{:})
+prevOut = p.Results.prevOut;
+velocityInsteadReward = p.Results.velocityInsteadReward  ;
+numCorrectiveSaccadesInsteadOfReward = p.Results.numCorrectiveSaccadesInsteadOfReward;
+
+
+[response,ind,ts] = data2response(data,epoch);
+
+[groups, group_names] = createGroups(data,epoch,ind,prevOut,velocityInsteadReward,...
+    numCorrectiveSaccadesInsteadOfReward);
+
+groups{1} = randPermute(groups{1});
+groups{3} = randPermute(groups{3});
+
+inxForXAve = {find(groups{1}==25 & groups{3}==0),...
+    find(groups{1}==25 & groups{3}==1),...
+    find(groups{1}==75 & groups{3}==0),...
+    find(groups{1}==75 & groups{3}==1)};
+
+contrastWeights = [3,-1,-1,-1];
+ns = cellfun(@length,inxForXAve);
+
+for t=1:length(ts)
+
+    if mean(response(t,:))<MINIMAL_RATE_IN_BIN
+        low = 1;
+    end
+
+    [~, tbl ] =  calOmegaSquare(response(t,:),groups, group_names,'partial',true,...
+        'includeTime',false);
+
+    msw = tbl{end-1,5};
+    N = length(response(t,:));
+
+    for i=1:length(inxForXAve)
+        xAve(i) = mean(response(t,inxForXAve{i}));
+    end
+    ssc =(contrastWeights*xAve')^2/sum((contrastWeights.^2)./ns);
+    
+    % df of effect size is 1. 
+    effectSizes(t) = (ssc-msw)/(ssc+(N-1)*msw);
+
+end
+
+
+end
+
